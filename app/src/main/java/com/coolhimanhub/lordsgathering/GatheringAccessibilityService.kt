4bc5f4be78d6dd7294eccd58ab389b30307347cb
CoolHimanHub <coolhiman18@gmail.com>
2026-09-07 17:29:39 +0530

    Create GatheringAccessibilityService.kt

---
+++ app/src/main/java/com/coolhimanhub/lordsgathering/GatheringAccessibilityService.kt
@@ -0,0 +1,34 @@
+package com.coolhimanhub.lordsgathering
+
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.view.accessibility.AccessibilityEvent
+
+class GatheringAccessibilityService : AccessibilityService() {
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        // Accessibility events will be handled here.
+    }
+
+    override fun onInterrupt() {
+        // Called when the service is interrupted.
+    }
+
+    fun tap(x: Float, y: Float) {
+        val path = Path()
+        path.moveTo(x, y)
+
+        val gesture = GestureDescription.Builder()
+            .addStroke(
+                GestureDescription.StrokeDescription(
+                    path,
+                    0,
+                    100
+                )
+            )
+            .build()
+
+        dispatchGesture(gesture, null, null)
+    }
+}
