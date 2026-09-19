f8509fddbcc533dfffb14bee5d09b8ca751e1401
CoolHimanHub <coolhiman18@gmail.com>
2026-09-20 00:12:32 +0530

    Test affine calibration stability checks

---
+++ app/src/test/java/com/coolhiman/lordsassistant/map/AffineGridCalibratorStabilityTest.kt
@@ -0,0 +1,28 @@
+package com.coolhiman.lordsassistant.map
+
+import com.coolhiman.lordsassistant.model.ScreenPoint
+import com.coolhiman.lordsassistant.model.WorldCoordinate
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class AffineGridCalibratorStabilityTest {
+    @Test
+    fun rejectsOneDimensionalCalibration() {
+        val calibrator = AffineGridCalibrator()
+        calibrator.addSample(WorldCoordinate(355, 10, 20), ScreenPoint(100f, 200f))
+        calibrator.addSample(WorldCoordinate(355, 11, 20), ScreenPoint(120f, 200f))
+        calibrator.addSample(WorldCoordinate(355, 12, 20), ScreenPoint(140f, 200f))
+        assertNull(calibrator.fit())
+    }
+
+    @Test
+    fun acceptsTwoDimensionalCalibration() {
+        val calibrator = AffineGridCalibrator()
+        calibrator.addSample(WorldCoordinate(355, 10, 20), ScreenPoint(100f, 200f))
+        calibrator.addSample(WorldCoordinate(355, 11, 20), ScreenPoint(120f, 220f))
+        calibrator.addSample(WorldCoordinate(355, 10, 21), ScreenPoint(80f, 220f))
+        calibrator.addSample(WorldCoordinate(355, 11, 21), ScreenPoint(100f, 240f))
+        assertNotNull(calibrator.fit())
+    }
+}
