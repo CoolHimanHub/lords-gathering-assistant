2a445a3b7abc7e2dc33043d097899e4d946b4e42
CoolHimanHub <coolhiman18@gmail.com>
2026-09-19 07:54:24 +0530

    Add prototype source: app/src/main/java/com/coolhiman/lordsassistant/accessibility/LmAccessibilityService.kt

---
+++ app/src/main/java/com/coolhiman/lordsassistant/accessibility/LmAccessibilityService.kt
@@ -0,0 +1,24 @@
+package com.coolhiman.lordsassistant.accessibility
+
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.view.accessibility.AccessibilityEvent
+
+class LmAccessibilityService : AccessibilityService() {
+    companion object { @Volatile var instance: LmAccessibilityService? = null }
+
+    override fun onServiceConnected() { super.onServiceConnected(); instance = this }
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) = Unit
+    override fun onInterrupt() = Unit
+
+    fun tap(x: Float, y: Float): Boolean {
+        val path = Path().apply { moveTo(x, y) }
+        val gesture = GestureDescription.Builder()
+            .addStroke(GestureDescription.StrokeDescription(path, 0, 80))
+            .build()
+        return dispatchGesture(gesture, null, null)
+    }
+
+    override fun onDestroy() { instance = null; super.onDestroy() }
+}
