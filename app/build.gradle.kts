808ee290665e998820d09cf60ff587c8d3f4062f
CoolHimanHub <coolhiman18@gmail.com>
2026-09-19 10:35:35 +0530

    Add unit test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,4 +31,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.mlkit:text-recognition:16.0.1")
     implementation("org.opencv:opencv:4.14.0")
+    testImplementation("junit:junit:4.13.2")
 }
