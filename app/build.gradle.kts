28237dfff7e8cdfa7da2dd0961043f4269477c7d
CoolHimanHub <coolhiman18@gmail.com>
2026-09-21 00:54:39 +0530

    Set app version to V0.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.coolhiman.lordsassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 71
-        versionName = "0.7.1"
+        versionCode = 72
+        versionName = "0.7.2"
     }
     buildTypes {
         release {
