8385eba9670fc2bbdce97468b929330aa4c5da95
CoolHimanHub <coolhiman18@gmail.com>
2026-09-21 09:26:20 +0530

    Restore missing march assertion import

--- app/src/test/java/com/coolhiman/lordsassistant/target/ActionMarchAssociationTrackerTest.kt
+++ app/src/test/java/com/coolhiman/lordsassistant/target/ActionMarchAssociationTrackerTest.kt
@@ -3,6 +3,7 @@ package com.coolhiman.lordsassistant.target
 import com.coolhiman.lordsassistant.model.ScreenPoint
 import com.coolhiman.lordsassistant.vision.MarchSignal
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
