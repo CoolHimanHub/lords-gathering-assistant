4f9da0ce3121ca92cc3fd557311de3bcdf1fdabb
CoolHimanHub <coolhiman18@gmail.com>
2026-09-21 12:56:10 +0530

    V0.9.5: align Android version metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.coolhiman.lordsassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 94
-        versionName = "0.9.4"
+        versionCode = 95
+        versionName = "0.9.5"
     }
     buildTypes {
         release {
