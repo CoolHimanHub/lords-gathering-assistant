08ff581eceabad88798eb4de1bdce9a0ba41b818
CoolHimanHub <coolhiman18@gmail.com>
2026-09-21 11:36:55 +0530

    V0.8.6: align Android version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.coolhiman.lordsassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 85
-        versionName = "0.8.5"
+        versionCode = 86
+        versionName = "0.8.6"
     }
     buildTypes {
         release {
