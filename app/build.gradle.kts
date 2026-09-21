58ec52e128fd9ebe8c991db1ea11ca054ff06167
CoolHimanHub <coolhiman18@gmail.com>
2026-09-21 17:41:50 +0530

    V0.10: prepare device validation build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 100
-        versionName = "0.9.10"
+        versionName = "0.10.0"
     }
     buildTypes {
         release {
