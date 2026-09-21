376a7258d76e4176ca5aa3a52acdef86649a6d26
CoolHimanHub <coolhiman18@gmail.com>
2026-09-21 10:09:09 +0530

    Run Android geometry test with Robolectric

--- app/src/test/java/com/coolhiman/lordsassistant/vision/DetectionFusionTest.kt
+++ app/src/test/java/com/coolhiman/lordsassistant/vision/DetectionFusionTest.kt
@@ -7,7 +7,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class DetectionFusionTest {
     @Test
     fun marchNearTileMarksItOccupiedAndIncoming() {
