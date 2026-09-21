52bc8ae01b44b79b9b72b30feb5e57f5e6f05ae4
CoolHimanHub <coolhiman18@gmail.com>
2026-09-21 10:09:06 +0530

    Run Android geometry test with Robolectric

--- app/src/test/java/com/coolhiman/lordsassistant/target/ActionButtonDetectorTest.kt
+++ app/src/test/java/com/coolhiman/lordsassistant/target/ActionButtonDetectorTest.kt
@@ -7,7 +7,10 @@ import com.coolhiman.lordsassistant.vision.TextRegion
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class ActionButtonDetectorTest {
     @Test
     fun gatherTextBecomesResourceActionWhenPopupAnchorIsNear() {
