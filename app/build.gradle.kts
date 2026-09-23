86016b170dcd5ab8036e47fea3ac5a7ba2959363
CoolHimanHub <coolhiman18@gmail.com>
2026-09-24 00:08:37 +0530

    V2.0.0: promote companion to product release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.coolhiman.lordsassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 103
-        versionName = "1.0.2"
+        versionCode = 200
+        versionName = "2.0.0"
     }
 
     val stableKeystorePath = System.getenv("LM_DEBUG_KEYSTORE")
