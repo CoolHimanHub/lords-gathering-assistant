9ab6209beab37c26c8723686418d76c2e392f9b2
CoolHimanHub <coolhiman18@gmail.com>
2026-09-25 23:14:17 +0530

    V2.5: bump runtime version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.coolhiman.lordsassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 241
-        versionName = "2.4.1"
+        versionCode = 250
+        versionName = "2.5.0"
     }
 
     val stableKeystorePath = System.getenv("LM_DEBUG_KEYSTORE")
